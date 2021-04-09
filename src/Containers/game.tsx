bddd596e95e24b50bf7cf4e66b081fc58694600c
Vladyslav Soldatenko <teenwolf23299@gmail.com>
2021-04-09 16:23:19 +0300

    card rendering

--- src/Containers/game.tsx
+++ src/Containers/game.tsx
@@ -1,16 +1,47 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../components/card';
-import Wrap from '../hoc/wrap';
 import style from './game.module.css';
+import getPokemonsByIds from '../cardsLogic/cards';
 
-class Game extends Component<{}, {}> {
-  render() {
-    return (
-      <div className={style.gameBoard}>
-        <Card /> <Card /> <Card /> <Card /> <Card />
-      </div>
-    );
-  }
+type card = {
+  imgLink: string;
+  id: number;
+  name: string;
+  isClicked: boolean;
+};
+function Game() {
+  const [cards, setCards] = useState<card[] | null>(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const populateCards = async () => {
+      let pokemons = await Promise.all(getPokemonsByIds(7));
+      let cardsState: card[] = pokemons.map((pokemon) => {
+        return {
+          id: pokemon.id,
+          name: pokemon.name,
+          imgLink: pokemon.imgLink,
+          isClicked: false,
+        };
+      });
+      setCards(cardsState);
+      setLoading(false);
+    };
+    populateCards();
+  }, []);
+
+  return (
+    <div className={style.gameBoard}>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : cards ? (
+        cards.map((card) => {
+          return <Card imgSrc={card.imgLink} id={card.id} name={card.name} />;
+        })
+      ) : null}
+    </div>
+  );
 }
 
 export default Game;
